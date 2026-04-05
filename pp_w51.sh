9eaeb9d10a85acbc8df087814ee7dd68db1a2073
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-04-05 12:36:39 -0400

    Additional script

---
+++ pp_w51.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=gs_51pp
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --output=./preprocess.txt
+#SBATCH --error=./preprocess.err
+
+source ~/.bashrc
+conda activate geo
+
+python process_dataset.py \
+    --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
+    --output-dir /work/zdc6/greenspace/data/w51 \
+    --window-size 51 \
