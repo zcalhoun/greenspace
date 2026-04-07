9434c0a08edbba4f5197d1401d7381ca9a8d44f3
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-04-07 11:40:38 -0400

    Updated scripts for 101 ws experiment

---
+++ w101_gs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=w101gs
+#SBATCH --array=0-21
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p scavenger-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --output=./w101gs/%a.txt
+#SBATCH --error=./w101gs/%a.err
+
+source ~/.bashrc
+conda activate svgp
+
+python main.py \
+    --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
+    --output-dir /work/zdc6/greenspace/results/w101/pm/gs/ \
+    --patience 3 \
+    --greenspace \
+    --batch-size 1024 \
+    --pretrain-lr 0.1 \
+    --window-size 101
---
+++ w101_ng.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=w101ng
+#SBATCH --array=0-21
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p scavenger-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --output=./w101ng/%a.txt
+#SBATCH --error=./w101ng/%a.err
+
+source ~/.bashrc
+conda activate svgp
+
+python main.py \
+    --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
+    --output-dir /work/zdc6/greenspace/results/w101/pm/ng/ \
+    --patience 3 \
+    --batch-size 1024 \
+    --pretrain-lr 0.1 \
+    --window-size 101
