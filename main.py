a4675bdd380bcc6bffcf0135fd68f4102c938a9d
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-04-07 12:33:18 -0400

    Removed a dependency

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import sys
 
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+
+# from matplotlib import pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.gaussian_process import GaussianProcessRegressor, kernels
 
