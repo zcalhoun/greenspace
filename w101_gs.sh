bcf9f608b67c9023f0c8609feff5155390e1fb25
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-04-13 16:46:56 -0400

    Added script for testing new configuration

--- w101_gs.sh
+++ w101_gs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=w101gs
+#SBATCH --job-name=amgs_w101
 #SBATCH --array=0-21
 #SBATCH --mail-user=zachary.calhoun@duke.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH -p carlsonlab-gpu
+#SBATCH -p scavenger-gpu
 #SBATCH --gres=gpu:1
-#SBATCH --mem=64G
-#SBATCH --output=./w101gs/%a.txt
-#SBATCH --error=./w101gs/%a.err
+#SBATCH --mem=8G
+#SBATCH --output=./amgs_w101/%a.txt
+#SBATCH --error=./amgs_w101/%a.err
 
 source ~/.bashrc
 conda activate svgp
@@ -20,4 +20,6 @@ python main.py \
     --greenspace \
     --batch-size 1024 \
     --pretrain-lr 0.1 \
-    --window-size 101
+    --lr 0.05 \
+    --window-size 101 \
+    --amp
