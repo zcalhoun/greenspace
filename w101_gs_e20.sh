a78d95a9eb2be76e504b41b86866edfc97f71af7
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-04-15 11:01:12 -0400

    Added experiment for more epochs

---
+++ w101_gs_e20.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=pm_gs_w101
+#SBATCH --array=0-5
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p scavenger-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8G
+#SBATCH --output=./e20_pm_gs_w101/%a.txt
+#SBATCH --error=./e20_pm_gs_w101/%a.err
+
+source ~/.bashrc
+conda activate svgp
+
+python main.py \
+    --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
+    --output-dir /work/zdc6/greenspace/results/w101_e20/pm/gs/ \
+    --patience 3 \
+    --greenspace \
+    --batch-size 128 \
+    --pretrain-lr 0.1 \
+    --lr 0.1 \
+    --window-size 101 \
+    --epochs 20 \
+    --pretrain-epochs 2
