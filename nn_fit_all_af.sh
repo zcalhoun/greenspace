91942a49ebaabb83a2c60e1ac6b88c18499ee3f8
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-04-27 16:46:27 -0400

    Created scripts to fit the data for morning and afternoon

---
+++ nn_fit_all_af.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=nn_af_fit
+#SBATCH --array=0-21
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p scavenger-gpu
+#SBATCH --gres=gpu:2080:1
+#SBATCH --mem=8G
+#SBATCH --output=./nn_af_fit/%a.txt
+#SBATCH --error=./nn_af_fit/%a.err
+
+source ~/.bashrc
+conda activate svgp
+
+python create_maps.py \
+    --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
+    --output-dir /work/zdc6/greenspace/results/ridge/af_ip100_nn/ \
+    --time af \
+    --patience 5 \
+    --batch-size 64 \
+    --pretrain-lr 0.1 \
+    --lr 0.1 \
+    --window-size 201 \
+    --epochs 20 \
+    --pretrain-epochs 10 \
+    --num-inducing-points 100 \
+    --non-negative
