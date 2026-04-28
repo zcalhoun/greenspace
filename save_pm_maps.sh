c88180a7612c02db03805c091cf7007fddfe920f
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-04-28 08:45:11 -0400

    Script to create maps

---
+++ save_pm_maps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=nn_save_pm
+#SBATCH --array=0
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --output=./nn_save_pm/%a.txt
+#SBATCH --error=./nn_save_pm/%a.err
+
+source ~/.bashrc
+conda activate svgp
+
+python create_maps.py \
+    --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
+    --output-dir /work/zdc6/greenspace/results/ridge/pm_ip100_nn/
