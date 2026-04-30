7a54fece095ab38f54ff8cba02f6370c65d53132
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-04-30 15:00:54 -0400

    Create afternoon and morning maps scripts

---
+++ save_af_maps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=nn_save_af
+#SBATCH --array=1-21
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --output=./nn_save_af/%a.txt
+#SBATCH --error=./nn_save_af/%a.err
+
+source ~/.bashrc
+conda activate svgp
+
+python save_maps.py \
+    --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
+    --output-dir /work/zdc6/greenspace/results/ridge/af_ip100_nn/ \
+    --time af
---
+++ save_am_maps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=nn_save_am
+#SBATCH --array=1-21
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --output=./nn_save_am/%a.txt
+#SBATCH --error=./nn_save_am/%a.err
+
+source ~/.bashrc
+conda activate svgp
+
+python save_maps.py \
+    --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
+    --output-dir /work/zdc6/greenspace/results/ridge/am_ip100_nn/ \
+    --time am
