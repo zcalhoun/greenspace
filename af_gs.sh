36f4a0965e852a4203894fda00398a62de8ab556
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-05-05 16:53:03 -0400

    Added af and am gs experiments

---
+++ af_gs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=af_gs
+#SBATCH --array=0-21
+#SBATCH --mail-user=zachary.calhoun@duke.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -p carlsonlab-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH --output=./af_gs/%a.txt
+#SBATCH --error=./af_gs/%a.err
+
+source ~/.bashrc
+conda activate svgp
+
+python main.py \
+    --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
+    --output-dir /work/zdc6/greenspace/results/af/gs/ \
+    --epochs 100 \
+    --lr 0.01 \
+    --batch-size 128 \
+    --window-size 500 \
+    --bayes-opt-iters 50 \
+    --greenspace \
+    --gs-downsample 10 \
+    --num-inducing-points 100 \
+    --time af
