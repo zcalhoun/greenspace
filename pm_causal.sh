2a210642bef6ad8f9e1d077523fe6a35950711c4
Zach Calhoun <zcalhoun.nc@gmail.com>
2026-05-07 13:54:11 -0400

    Results B

--- pm_causal.sh
+++ pm_causal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=pm_causal
+#SBATCH --job-name=pm_causal_b
 #SBATCH --array=0-21
 #SBATCH --mail-user=zachary.calhoun@duke.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -15,7 +15,7 @@ conda activate svgp
 
 python causal_estimates.py \
     --data-dir /hpc/group/carlsonlab/zdc6/greenspace/data/traversals/ \
-    --output-dir /work/zdc6/greenspace/results/pm_causal/ \
+    --output-dir /work/zdc6/greenspace/results/pm_causal_b/ \
     --epochs 100 \
     --lr 0.01 \
     --batch-size 128 \
@@ -23,6 +23,6 @@ python causal_estimates.py \
     --gs-downsample 10 \
     --num-inducing-points 100 \
     --lengthscale 120 \
-    --l2-penalty 0.001 \
+    --l2-penalty 0.01 \
     --spatial-mask-km 10.0 \
     --feature-mask-quantile 0.999
